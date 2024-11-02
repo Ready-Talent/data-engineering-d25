eee2b85058d0ff11697aa11fbd1774ac9a75cc63
SaifAmirr <saif.1000.ebrahim@gmail.com>
2024-11-02 14:01:51 +0000

    updated

--- dags/SaifAmir/Dag_SaifAmir.py
+++ dags/SaifAmir/Dag_SaifAmir.py
@@ -1,54 +1,35 @@
-
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from datetime import datetime
+from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
+from airflow.utils.dates import days_ago
 
-default_args = {
-    'retries': 1
-    
-}
+# Define the DAG
+with DAG(
+    dag_id='postgres_to_bigquery_via_gcs',
+    schedule_interval=None,
+    description ='Loading Data from postgres to bigquery via gcs'
+) as dag:
 
-dag1 = DAG(
-    'test_dag',
-    default_args=default_args,
-    start_date=datetime(2024, 4, 20),
-    description='creating a test DAG',
-    schedule_interval=None
-)
+    # transfer data from PostgreSQL to GCS
+    transfer_to_gcs = PostgresToGCSOperator(
+        task_id='transfer_postgres_to_gcs',
+        postgres_conn_id='postgres_conn',  # Your PostgreSQL connection ID
+        sql='SELECT * FROM orders;',  # Your SQL query
+        bucket='ready-d25-postgres-to-gcs',  # Your GCS bucket name
+        filename='seif/orders.csv',  # GCS object path
+        export_format='csv',  # Export format (csv, json, etc.)
+    )
 
-def print_one():
-    return "hello world"
+    # load data from GCS to BigQuery
+    load_to_bigquery = GCSToBigQueryOperator(
+        task_id='load_gcs_to_bigquery',
+        bucket='ready-d25-postgres-to-gcs',  # Your GCS bucket name
+        source_objects=['seif/orders.csv'],  # GCS object path
+        destination_project_dataset_table='ready-de-25.airflow_transfers.orders',  # BigQuery table
+        source_format='CSV',  # Format of the data in GCS
+        skip_leading_rows=1,  # Skip header row if necessary
+        autodetect=True,  # Automatically detect schema
+    )
 
-
-def print_two(a,b):
-    return a + b
-
-
-def print_three(date):
-    return date
-
-execution_date = '{{execution_date}}'
-
-task_number_one = PythonOperator(
-    task_id = "task_1",
-    python_callable = print_one,
-    dag = dag1   
-)
-
-task_number_two = PythonOperator(
-    task_id = "task_2",
-    python_callable = print_two,
-    op_kwargs = {"a":1,"b":2},
-    dag = dag1   
-)
-
-task_number_three = PythonOperator(
-    task_id = "task_3",
-    python_callable = print_three,
-    op_kwargs = {"date":execution_date},
-    dag = dag1   
-)
-
-
-
-task_number_one >> task_number_two >> task_number_three
+    # Set task dependencies
+    transfer_to_gcs >> load_to_bigquery
