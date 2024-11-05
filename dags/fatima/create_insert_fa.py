4865346ce325f56ea1dcfcf0e719201198e110b4
fatima-adel <fatima.ramadan95@gmail.com>
2024-11-05 20:24:29 +0200

    fix indentation

--- dags/fatima/create_insert_fa.py
+++ dags/fatima/create_insert_fa.py
@@ -6,9 +6,8 @@ import os
 
 with DAG(
     'create_and_populate_bigquery_table',
-    start_date=datetime(2024, 1, 1),
-    schedule_interval=None,
-    catchup=False
+    start_date=datetime(2024, 4, 20),
+    schedule_interval=None
 ) as create_insert_dag:
 
     def create_table():
@@ -18,18 +17,18 @@ with DAG(
         table_ref = client.dataset(dataset_id).table(table_id)
 
 
-    # Get the current working directory
-    current_dir = os.getcwd()
+        # Get the current working directory
+        current_dir = os.getcwd()
 
-    # Join the current directory with a filename to create a full path
-    file_path = os.path.join(current_dir, "your_schema.json")
+        # Join the current directory with a filename to create a full path
+        file_path = os.path.join(current_dir, "your_schema.json")
 
-    # Load the JSON schema from a file or string
-    with open(file_path, 'r') as f:
-        schema_json = json.load(f)
+        # Load the JSON schema from a file or string
+        with open(file_path, 'r') as f:
+            schema_json = json.load(f)
 
-    table = bigquery.Table(table_ref, schema=schema_json)
-    table = client.create_table(table)  # API request
+        table = bigquery.Table(table_ref, schema=schema_json)
+        table = client.create_table(table)  # API request
 
     
     create_table_task = PythonOperator(
