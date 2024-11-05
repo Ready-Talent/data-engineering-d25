b45d9328d74e8de6f846cacc79bae04ebac55ff4
megwed50@gmail.com <megwed50@gmail.com>
2024-11-05 20:25:46 +0200

    sixthEdit

--- dags/Abdelgawad/create_table_abdelgawad.py
+++ dags/Abdelgawad/create_table_abdelgawad.py
@@ -41,8 +41,6 @@ load_data = BigQueryInsertJobOperator(
         "query": {
             "query": """
                 INSERT INTO `ready-de-25.airflow_star_schema.dim_customer_abdelgawad` 
-                (customer_id, name, email, address, phone, created_at_timestamp, updated_at_timestamp)
-                SELECT customer_id, name, email, address, phone, created_at_timestamp, updated_at_timestamp
                 FROM `ready-de-25.ecommerce.customers`
             """,
             "useLegacySql": False, 
