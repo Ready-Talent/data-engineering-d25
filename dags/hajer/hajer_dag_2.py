9b88fb7ebf65e90e3b33f0a4e83c4c115fe770a1
Hajer-Z <hajerzakaria@gmail.com>
2024-11-05 18:29:44 +0200

    late commit

--- dags/hajer/hajer_dag_2.py
+++ dags/hajer/hajer_dag_2.py
@@ -1,44 +1,37 @@
 from airflow import DAG
 from airflow.providers.google.cloud.transfers.postgres_to_gcs import PostgresToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
-from datetime import datetime
 
 default_args = {
     'retries': 1
 }
 
-
-dag1 = DAG(
-    'postgres_to_bigquery_hajer',
+with DAG(
+    'postgres_to_bq-hajer',
+    describtion='creating transfer dag',
     default_args=default_args,
-    start_date=datetime(2024, 11, 1),
-    description='creating transfer dag',
     schedule_interval=None,
-    catchup=False,
-)
+) as dag:
 
+    postgres_to_gcs = PostgresToGCSOperator(
+        task_id="export_postgres_to_gcs",
+        sql="select * from orders",
+        postgres_conn_id='postgres_conn',
+        bucket='ready-d25-postgres-to-gcs',
+        filename="hajer/orders.csv",
+        export_format='csv',   
+    )
 
-postgres_to_gcs_1 = PostgresToGCSOperator(
-    task_id="export_postgres_to_gcs",
-    sql="select * from orders",
-    postgres_conn_id="postgres_conn",
-    bucket="ready-d25-postgres-to-gcs",
-    filename="hajer/orders.csv",
-    export_format="csv",
-    dag = dag1   
-)
+    gcs_to_bigquery = GCSToBigQueryOperator(
+        task_id='load_csv_to_bigquery',
+        bucket='ready-d25-postgres-to-gcs',
+        source_objects='hajer/orders.csv',
+        destination_project_dataset_table='ready-de-25.airflow_transfers.orders_hajer',
+        source_format='csv',
+        skip_leading_rows=1,
+        autodetect=True,
+        create_disposition='CREATE_IF_NEEDED',
+    )
 
-gcs_to_bigquery_2 = GCSToBigQueryOperator(
-    task_id="load_csv_to_bigquery",
-    bucket="ready-d25-postgres-to-gcs",  # Same GCS bucket name
-    source_objects=["hajer/orders.csv"],  # List of files to load
-    destination_project_dataset_table="ready-de-25.airflow_transfers.orders_hajer",  # BigQuery destination
-    source_format="CSV",  # Specify the source format
-    skip_leading_rows=1,  # Skip header row if present
-    autodetect=True,  # Enable schema auto-detection
-    create_disposition="CREATE_IF_NEEDED",
-    dag = dag1
-)
-
-postgres_to_gcs_1 >> gcs_to_bigquery_2
+    postgres_to_gcs >> gcs_to_bigquery
 
